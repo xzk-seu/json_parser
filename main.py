d55a69571924fb5a57ac1669f0f030744621932c
xzk-laptop <xzk09014134@outlook.com>
2018-10-17 11:39:43 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import json
+
+
+def json_parse(name, temp_dict):
+    # "dfn": "Blockchain"
+    # if temp_dict.get("dfn") == "Blockchain":
+    #     print('find in', name)
+    #     return True
+    for k, v in temp_dict.items():
+        if v == "Blockchain":
+            print('find in', k)
+            print(name)
+            return True
+        if isinstance(v, dict):
+            if json_parse(k, v):
+                print(k)
+        if isinstance(v, list):
+            for n, i in enumerate(v):
+                if isinstance(i, dict):
+                    if json_parse('%s[%d]' % (k, n), i):
+                        print(name)
+    return False
+
+
+if __name__ == '__main__':
+    with open('cs.json', 'r', encoding='UTF-8') as fr:
+        js_dict = json.load(fr)
+
+    if json_parse('root', js_dict):
+        print('T')
+    else:
+        print('F')
+
+    with open('cs.json', 'w', encoding='UTF-8') as fw:
+        json.dump(js_dict, fw, indent=4)
